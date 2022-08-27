5bf7dedfc22b01357ff8e3321630d23581565a23
shamimspro <shamimspro@gmail.com>
2022-08-27 19:07:32 +0600

    practice

--- practice/posts.js
+++ practice/posts.js
@@ -12,3 +12,20 @@ function display(array) {
     }
 }
 loadData();
+
+function postData() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => display(json));
+}
+
